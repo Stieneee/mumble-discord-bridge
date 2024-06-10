26efbc1ff3032c8c08f2f3cf098ed09bcd919783
Tyler Stiene <tystiene@gmail.com>
2024-06-09 22:41:39 -0400

    clean up mumble streams when a users disconnects

--- go.mod
+++ go.mod
@@ -7,18 +7,19 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/prometheus/client_golang v1.19.1
 	github.com/stieneee/gopus v0.0.0-20210424193312-6d10f6090335
-	github.com/stieneee/gumble v0.0.0-20240606033158-72cc4679195e
+	github.com/stieneee/gumble v0.0.0-20240610021017-a3449ae7108c
 	github.com/stieneee/tickerct v0.0.0-20210420020607-d1b092aa40e9
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/prometheus/client_model v0.5.0 // indirect
-	github.com/prometheus/common v0.48.0 // indirect
-	github.com/prometheus/procfs v0.12.0 // indirect
-	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/sys v0.17.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/gorilla/websocket v1.5.2 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.54.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 )
--- internal/bridge/mumble.go
+++ internal/bridge/mumble.go
@@ -14,32 +14,28 @@ import (
 
 // MumbleDuplex - listener and outgoing
 type MumbleDuplex struct {
-	mutex              sync.Mutex
-	fromMumbleArr      []chan gumble.AudioBuffer
-	mumbleStreamingArr []bool
-	mumbleSleepTick    sleepct.SleepCT
+	mutex           sync.Mutex
+	streams         []chan gumble.AudioBuffer
+	mumbleSleepTick sleepct.SleepCT
 }
 
 func NewMumbleDuplex() *MumbleDuplex {
 	return &MumbleDuplex{
-		fromMumbleArr:      make([]chan gumble.AudioBuffer, 0),
-		mumbleStreamingArr: make([]bool, 0),
-		mumbleSleepTick:    sleepct.SleepCT{},
+		streams:         make([]chan gumble.AudioBuffer, 0),
+		mumbleSleepTick: sleepct.SleepCT{},
 	}
 }
 
 // OnAudioStream - Spawn routines to handle incoming packets
 func (m *MumbleDuplex) OnAudioStream(e *gumble.AudioStreamEvent) {
 
-	// hold a reference ot the channel in the closure
-	streamChan := make(chan gumble.AudioBuffer, 100)
+	stream := make(chan gumble.AudioBuffer, 100)
 
 	m.mutex.Lock()
-	m.fromMumbleArr = append(m.fromMumbleArr, streamChan)
-	m.mumbleStreamingArr = append(m.mumbleStreamingArr, false)
+	m.streams = append(m.streams, stream)
 	m.mutex.Unlock()
 
-	promMumbleArraySize.Set(float64(len(m.fromMumbleArr)))
+	promMumbleArraySize.Set(float64(len(m.streams)))
 
 	go func() {
 		name := e.User.Name
@@ -49,12 +45,23 @@ func (m *MumbleDuplex) OnAudioStream(e *gumble.AudioStreamEvent) {
 
 			// 480 per 10ms
 			for i := 0; i < len(p.AudioBuffer)/480; i++ {
-				streamChan <- p.AudioBuffer[480*i : 480*(i+1)]
+				stream <- p.AudioBuffer[480*i : 480*(i+1)]
 			}
 			promReceivedMumblePackets.Inc()
 			m.mumbleSleepTick.Notify()
 		}
 		log.Println("Mumble audio stream ended", name)
+		//remove the MumbleStream from the array
+		m.mutex.Lock()
+		defer m.mutex.Unlock()
+		for i := 0; i < len(m.streams); i++ {
+			if m.streams[i] == stream {
+				m.streams = append(m.streams[:i], m.streams[i+1:]...)
+				break
+			}
+		}
+
+		promMumbleArraySize.Set(float64(len(m.streams)))
 	}()
 }
 
@@ -83,22 +90,12 @@ func (m *MumbleDuplex) fromMumbleMixer(ctx context.Context, cancel context.Cance
 		streamingCount := 0
 
 		// Work through each channel
-		for i := 0; i < len(m.fromMumbleArr); i++ {
-			if len(m.fromMumbleArr[i]) > 0 {
+		for i := 0; i < len(m.streams); i++ {
+			if len(m.streams[i]) > 0 {
 				sendAudio = true
-				if !m.mumbleStreamingArr[i] {
-					m.mumbleStreamingArr[i] = true
-					streamingCount++
-					// log.Println("Mumble starting", i)
-				}
-
-				x1 := (<-m.fromMumbleArr[i])
+				streamingCount++
+				x1 := (<-m.streams[i])
 				internalMixerArr = append(internalMixerArr, x1)
-			} else {
-				if m.mumbleStreamingArr[i] {
-					m.mumbleStreamingArr[i] = false
-					// log.Println("Mumble stopping", i)
-				}
 			}
 		}
 
